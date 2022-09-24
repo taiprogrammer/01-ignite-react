a6926a1144bcd99e24d1c3aaa1961a77def8d72d
taiprogrammer <taiza.marques@sptech.school>
2022-09-24 15:44:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./global.css";
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      {/* <div className={styles.wrapper}>ALO</div> */}
+    </>
+  );
+}
