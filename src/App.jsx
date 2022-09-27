b8ceb94f6971128b59722518a6e1008bc0d91fdf
taiprogrammer <taiza.marques@sptech.school>
2022-09-27 18:48:02 -0300

    post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./global.css";
 import styles from "./App.module.css";
-import { Header } from "./components/Header";
+
 import { Post } from "./components/Post";
+import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
 export function App() {
@@ -11,9 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Taiza" subscription="My post" />
-          <Post author="Taiza" subscription="My post" />
-          <Post author="Taiza" subscription="My post" />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,38 @@
-export function Post({ author, subscription }) {
+import styles from "./Post.module.css";
+
+export function Post() {
   return (
-    <div>
-      <h1>{author}</h1>
-      <p>{subscription}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/taiprogrammer.png"
+            alt="Author Icon"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Taiza Marques</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 11:13" dateTime="2022-05-11 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
+        </p>
+        <p>
+          <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
   );
 }
