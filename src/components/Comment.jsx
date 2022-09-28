a5dac9c5e32608d62c5beb425ab3e830bd96759e
taiprogrammer <taiza.marques@sptech.school>
2022-09-28 17:40:50 -0300

    comment list component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://www.github.com/taiprogrammer.png" alt="Avatar" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Taiza Marques</strong>
+              <time title="11 de Maio às 11:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Excluir comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
